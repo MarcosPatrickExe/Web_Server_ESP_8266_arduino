df59895bf48a28eb63f82d7f3f536cd9a2d70ee5
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2024-02-03 17:59:23 -0300

    Update Web_Server_ESP_8266.cpp

--- Web_Server_ESP_8266.cpp
+++ Web_Server_ESP_8266.cpp
@@ -4,8 +4,8 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
-const char* ssid = "Aqui Telecom Saraiva";
-const char* password = "91230777";
+const char* ssid = "my_local_network_name";
+const char* password = "12345678";
 
 String waitingEllipsis = "...";
 
